8381c3e7793c466b5735f4d69720da9530a67ef1
theodoros-git <kossitheodore@yahoo.com>
2021-11-04 13:38:44 +0100

    timezone set successfully

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module Tweete
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.generators faire | g |
+      # La description dans ces deux lignes crée un paramètre qui n'est pas généré automatiquement.
+      g.actifs faux
+      g.helper false
+    finir
+    config.time_zone = 'Tokyo'
+    config.active_record.default_ timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
